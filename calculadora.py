342c6d09c4c86d46a73df922427bc78f32553a01
Horus Solutions <brunodavidrocha@gmail.com>
2023-05-20 20:16:22 -0300

    Calculadora versao 2.0
    Criação de funções separadas para cada tarefa lógica
    Tratamento de erro com ValueError
    Adição de um bloco try-except para capturar exceções e exibir mensagens de erro claras.
    Adição de um bloco try-except para capturar exceções e exibir mensagens de erro claras.
    Adição de um bloco try-except para capturar exceções e exibir mensagens de erro claras.

--- calculadora.py
+++ calculadora.py
@@ -15,32 +15,51 @@ def divisao(a, b):
     else:
         return "Erro: divisão por zero!"
 
-print("Calculadora Simples")
-print("Operações disponíveis:")
-print("1 - Soma (+)")
-print("2 - Subtração (-)")
-print("3 - Multiplicação (*)")
-print("4 - Divisão (/)")
+def imprimir_menu():
+    print("Calculadora Simples")
+    print("Operações disponíveis:")
+    print("1 - Soma (+)")
+    print("2 - Subtração (-)")
+    print("3 - Multiplicação (*)")
+    print("4 - Divisão (/)")
+    
+def obter_operacao():
+    operacao = input("Selecione a operação desejada (1/2/3/4): ")
+    if operacao not in ["1", "2", "3", "4"]:
+        raise ValueError("Operação inválida!")
+    return operacao
 
-operacao = input("Selecione a operação desejada (1/2/3/4): ")
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
+def obter_numeros():
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    return num1, num2
 
-if operacao == "1":
-    resultado = soma(num1, num2)
-    simbolo = "+"
-elif operacao == "2":
-    resultado = subtracao(num1, num2)
-    simbolo = "-"
-elif operacao == "3":
-    resultado = multiplicacao(num1, num2)
-    simbolo = "*"
-elif operacao == "4":
-    resultado = divisao(num1, num2)
-    simbolo = "/"
-else:
-    print("Operação inválida!")
-    resultado = None
+def calcular(operacao, num1, num2):
+    if operacao == "1":
+        resultado = soma(num1, num2)
+        simbolo = "+"
+    elif operacao == "2":
+        resultado = subtracao(num1, num2)
+        simbolo = "-"
+    elif operacao == "3":
+        resultado = multiplicacao(num1, num2)
+        simbolo = "*"
+    elif operacao == "4":
+        resultado = divisao(num1, num2)
+        simbolo = "/"
+    return resultado, simbolo
 
-if resultado is not None:
+def exibir_resultado(num1, simbolo, num2, resultado):
     print(f"\n{num1} {simbolo} {num2} = {resultado}")
+
+def executar_calculadora():
+    try:
+        imprimir_menu()
+        operacao = obter_operacao()
+        num1, num2 = obter_numeros()
+        resultado, simbolo = calcular(operacao, num1, num2)
+        exibir_resultado(num1, simbolo, num2, resultado)
+    except ValueError as error:
+        print(f"Erro: {str(error)}")
+
+executar_calculadora()
